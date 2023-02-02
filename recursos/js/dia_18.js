5c21a65062f60426c92acd12fb927f49fee14e9c
Evanan <evanansemprun@gmail.com>
2023-02-02 18:02:11 -0400

    dia 18

--- recursos/js/dia_18.js
+++ recursos/js/dia_18.js
@@ -25,3 +25,53 @@ document.write("<br>");
 let datos2 = `nombre alumnos es <b> ${alumnos["nombre"]} </b> <br>`;
 
 document.write(datos2);
+//---------------------------
+
+var miArreglo = ["John", 24];
+console.log(miArreglo);
+
+var estudiantes = ["Nora", "Gino", "Emily", "Juan"];
+console.log(estudiantes);
+
+var notas = [95, 67, 89, 56];
+console.log(notas);
+
+//---------------------------------------
+
+var listaDeEstudiantes = [["Nora", 97], ["Gino", 78]];
+console.log(listaDeEstudiantes);
+
+var listaDeProductos = [["Camisa", 5.67, "S134"], ["Celular", 250, "S356"], ["Zapatos", 36.7, "S678"]];
+console.log(listaDeProductos);
+
+var dato = [[3.4, 5.6, 3.2], [6.7, 4.5, 7.8], [5.6, 3.4, 7.8]];
+console.log(dato);
+
+
+//----------------------------------------------------------
+var miArreglo = [10, 20, 30];
+
+/*
+Arreglo:  [10, 20, 30]
+Índices:    0   1   2
+*/
+
+console.log(miArreglo[0]); // 10
+console.log(miArreglo[1]); // 20
+console.log(miArreglo[2]); // 30
+
+var suma = miArreglo[0] + miArreglo[1] + miArreglo[2];
+console.log(suma);
+
+//----------------------------------------------
+var miArreglo = [10, 20, 30];
+
+console.log(miArreglo);
+miArreglo[0] = 40;
+console.log(miArreglo);
+
+miArreglo[1] = "Hola";
+console.log(miArreglo);
+
+miArreglo[1] = [1, 2, 3];
+console.log(miArreglo);
