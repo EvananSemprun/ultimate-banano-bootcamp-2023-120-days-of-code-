fb89472fe74265a4bc82e3715c38efac14398650
Evanan <evanansemprun@gmail.com>
2023-02-06 10:50:41 -0400

    dia 21

--- recursos/js/dia_21.js
+++ recursos/js/dia_21.js
@@ -1,5 +1,7 @@
 /*
-
+Las funciones en JavaScript son bloques de código reutilizables que se pueden invocar en cualquier momento para realizar una tarea específica. 
+Se definen mediante la palabra clave "function" y pueden aceptar argumentos o parámetros opcionales. 
+Las funciones también pueden devolver un valor utilizando la palabra clave "return".
 
 */
 
@@ -70,6 +72,21 @@ mostrarMiNombre();
 console.log(miNombre);
 
 //--------------------------------------------------------
+/*
+
+return es una palabra clave en JavaScript que se utiliza dentro de una función para devolver un valor a quien la llamó. 
+Cuando una función alcanza una declaración return, se detiene la ejecución de la función y se devuelve el valor especificado después de return. 
+
+return es útil para devolver un resultado o un valor calculado desde una función a quien la llamó. 
+Esto permite que la función realice un trabajo específico y devuelva un resultado que luego puede ser utilizado en otras partes del código. 
+Por ejemplo, una función puede realizar un cálculo complejo y devolver el resultado, lo que permite a la parte principal del programa ser más legible y mantenible.
+
+Además, return permite detener la ejecución de una función cuando se alcanza, lo que es útil para evitar la ejecución de código adicional innecesario. 
+Por ejemplo, una función puede realizar una verificación y si se cumple una condición, 
+puede devolver un valor y detener su ejecución, en lugar de continuar ejecutando el código adicional. Esto puede ser muy útil para mejorar la eficiencia del código.
+
+
+*/
 function sumar(a, b) {
     return a + b;
   }
