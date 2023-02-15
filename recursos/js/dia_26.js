716fd5b37d0c6a0004a2e24481211d67f55ecf31
Evanan <evanansemprun@gmail.com>
2023-02-15 18:49:27 -0400

    dia 26

--- recursos/js/dia_26.js
+++ recursos/js/dia_26.js
@@ -1,4 +1,5 @@
-// actividad 1
+// Ejercicio 1
+
 /*
 
 1) Se desea realizar una estadística de los pesos de los alumnos de un colegio, de acuerdo a la siguiente tabla:
@@ -11,47 +12,74 @@ Alumnos de más de 50 y menos de 60 Kg.
 
 Alumnos de más o igual a 60 Kg
 
+
+
 */
+function isNum(val) {
+  return !isNaN(val);
+} // permite verificar si es un numero 
+
+alert("Ejercicio 1");
 const menosde40 = [];
 const entre40y50 = [];
 const entre50y60 = [];
 const mas60 = [];
 
-
 var na = prompt("cuantos alumnos hay? ");
+if (isNum(na) == false) {
+  alert("error no es numero");
+} else {
+  if (isNum(ap) == false) {
+    alert("error no es numero");
+  } else {
+    for (i = 1; i <= na; i++) {
+      var ap = prompt("Agrega el peso del estudiante:  " + i);
 
-for (i=1; i<=na; i++) {
-  var ap = prompt("Agrega un peso: ");
+      if (ap < 40) {
+        menosde40.push("1");
+      } else if (ap >= 40 && ap <= 50) {
+        entre40y50.push("1");
+      } else if (ap > 50 && ap < 60) {
+        entre50y60.push("1");
+      } else {
+        mas60.push("1");
+      }
+    }
 
-if (ap < 40){
-  menosde40.push('1');
+    document.write(" <h1> Ejercicio 1 </h1> <br />");
+    document.write(
+      "Hay un total de " +
+        menosde40.length +
+        " alumno(s) con un peso menor de 40kg" +
+        "<br />"
+    );
 
-}else if (ap >= 40 && ap <= 50){
-  entre40y50.push('1');
+    document.write(
+      "Hay un total de " +
+        entre40y50.length +
+        " alumno(s) con un peso entre 40kg y 50kg " +
+        "<br />"
+    );
 
-}else if (ap > 50 && ap < 60){
-  entre50y60.push('1');
+    document.write(
+      "Hay un total de " +
+        entre50y60.length +
+        " alumno(s) con un peso de mayor a 50kg y menos a 60kg" +
+        "<br />"
+    );
 
-}else{
-  mas60.push('1');
+    document.write(
+      "Hay un total de " +
+        mas60.length +
+        " alumno(s) con un peso mayor a 60kg" +
+        "<br />" +
+        "<br />" +
+        "<br />"
+    );
+  }
 }
 
-}
-
-document.write("Hay un total de "+ menosde40.length + " alumno(s) con un peso menor de 40kg" + "<br />" );
-
-
-document.write("Hay un total de "+ entre40y50.length + " alumno(s) con un peso entre 40kg y 50kg "  + "<br />");
-
-
-document.write("Hay un total de "+ entre50y60.length + " alumno(s) con un peso de mayor a 50kg y menos a 60kg"  + "<br />");
-
-
-document.write("Hay un total de "+ mas60.length + " alumno(s) con un peso mayor a 60kg"  + "<br />" + "<br />" + "<br />" );
-
-
-// actividad 2
-
+// Ejercicio 2
 
 /*
 
@@ -63,18 +91,25 @@ La cuenta genera un interés del 15% anual, lo que equivale a 1.25% mensual.
 
 • El cálculo se requiere hasta el mes de diciembre.
 
+
 */
+
+alert("Ejercicio 2");
 const im = 250;
 const interes = 15;
 
 var ingresoi = parseInt(prompt("cuanto dinero desea depositar ? "));
 
-const itm = im * 11; // 2750
+if (isNum(ingresoi) == false) {
+  alert("error no es numero");
+} else {
+  const itm = im * 11; // 2750
 
-const it = itm + ingresoi;
+  const it = itm + ingresoi;
 
+  const total_de_interes = (interes * it) / 100;
+  const id = it + total_de_interes;
 
-const total_de_interes = interes * it / 100;
-const id = it + total_de_interes;
-
-document.write("el total es: "+ id)
+  document.write(" <h1> Ejercicio 2 </h1> <br /> <br />");
+  document.write("el total es: " + id + "<br />");
+}
