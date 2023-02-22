f0c363bf41b87243b87b0b66a698b2efa627914a
Evanan <evanansemprun@gmail.com>
2023-02-22 10:52:13 -0400

    dia 33

--- recursos/js/dia_33.js
+++ recursos/js/dia_33.js
@@ -1,15 +1,62 @@
-const cargarpelicula = async () => {
-  try {
-    const respuesta = await fetch(
-      "https://api.themoviedb.org/3/movie/550?api_key=e0ae0dba4628aa60e4768f5b77a8071c&language=es-MX"
-    );
-    console.log(respuesta);
+/*
 
-    const datos =await respuesta.json();
-    
-    console.log(datos);
-  } catch (error) {
-    console.log(error);
-  }
-};
-cargarpelicula();
+Los bloques try y catch en JavaScript nos permiten ejecutar código con la protección de excepciones. 
+Si ocurre una excepción, el código será interrumpido y se pasará al bloque catch asociado que realizará la acción deseada. 
+Esto nos permite controlar y manejar los errores producidos durante la ejecución de nuestro código.
+*/
+
+let pagina = 1;
+const btnAnterior = document.getElementById('btnAnterior');
+const btnSiguiente = document.getElementById('btnSiguiente');
+
+btnSiguiente.addEventListener('click', () => {
+	if(pagina < 1000){
+		pagina += 1;
+		cargarPeliculas();
+	}
+});
+
+btnAnterior.addEventListener('click', () => {
+	if(pagina > 1){
+		pagina -= 1;
+		cargarPeliculas();
+	}
+});
+
+const cargarPeliculas = async() => {
+	try {
+		const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${pagina}`);
+	
+		console.log(respuesta);
+
+		// Si la respuesta es correcta
+		if(respuesta.status === 200){
+			const datos = await respuesta.json();
+			
+			let peliculas = '';
+			datos.results.forEach(pelicula => {
+				peliculas += `
+					<div class="pelicula">
+						<img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+						<h3 class="titulo">${pelicula.title}</h3>
+					</div>
+				`;
+			});
+
+			document.getElementById('contenedor').innerHTML = peliculas;
+
+		} else if(respuesta.status === 401){
+			console.log('Pusiste la llave mal');
+		} else if(respuesta.status === 404){
+			console.log('La pelicula que buscas no existe');
+		} else {
+			console.log('Hubo un error y no sabemos que paso');
+		}
+
+	} catch(error){
+		console.log(error);
+	}
+
+}
+
+cargarPeliculas();
