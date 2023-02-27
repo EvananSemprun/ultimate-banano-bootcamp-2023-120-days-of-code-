82c7b194acae306977594c05db8100893af966e8
Evanan <evanansemprun@gmail.com>
2023-02-27 10:43:19 -0400

    dia 36

--- recursos/js/dia_36.js
+++ recursos/js/dia_36.js
@@ -22,3 +22,22 @@ modeSwitch.addEventListener("click", () => {
     modeText.innerText = "Modo oscuro";
   }
 });
+
+//----------------------------------
+document.addEventListener("keyup", e=>{
+
+    if (e.target.matches("#buscador")){
+  
+        if (e.key ==="Escape")e.target.value = ""
+  
+        document.querySelectorAll(".articulo").forEach(fruta =>{
+  
+            fruta.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+              ?fruta.classList.remove("filtro")
+              :fruta.classList.add("filtro")
+        })
+  
+    }
+  
+  
+  })
