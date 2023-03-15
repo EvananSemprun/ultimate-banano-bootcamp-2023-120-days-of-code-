ff61ee1814d139b508c8ef7c38f511d165d8a230
Evanan <evanansemprun@gmail.com>
2023-03-14 22:57:39 -0400

    dia 46 47 48 49 50

--- recursos/js/dia_46-47-48-49-50.js
+++ recursos/js/dia_46-47-48-49-50.js
@@ -20,3 +20,34 @@ window.addEventListener('resize', function(event){
   }
   
   adjustCardHeight();
+
+  //---------------
+  var bar = document.querySelector('.bar');
+  var button = document.querySelector('.bh');
+  var percent = document.querySelector('.percent');
+  
+  // Define el porcentaje de progreso inicial y la cantidad a incrementar por ciclo
+  var progress = 0;
+  var increment = 5;
+  
+  // Función para actualizar la barra de progreso y el porcentaje
+  function updateProgress() {
+    progress += increment;
+    if (progress > 90) {
+      progress = 90;
+    }
+    bar.style.width = progress + '%';
+    percent.textContent = progress + '%';
+    if (progress < 100) {
+      setTimeout(updateProgress, 500);
+    } else {
+      button.disabled = false;
+    }
+  }
+  
+  // Función para iniciar el progreso
+  function startProgress() {
+    progress = 0;
+    button.disabled = true;
+    updateProgress();
+  }
