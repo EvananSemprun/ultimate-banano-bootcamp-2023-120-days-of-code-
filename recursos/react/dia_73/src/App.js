5006c532734517c10fcbb57665c5056b3bf003df
EvananSemprun <evanansemprun@gmail.com>
2023-04-19 17:22:54 -0400

    dia_73

--- recursos/react/dia_73/src/App.js
+++ recursos/react/dia_73/src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import DateTimePicker from "./components/DateTimePicker";
 import "./App.css";
+import "./AppDarkMode.css";
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div className="app-container">
+    <div className={`app-container ${darkMode ? "dark-mode" : ""}`}>
       <h1 className="app-title">Selector de Fecha y Hora</h1>
       <DateTimePicker />
+      <button onClick={toggleDarkMode}>
+        {darkMode ? "Modo claro" : "Modo oscuro"}
+      </button>
     </div>
   );
 };
--- recursos/react/dia_73/src/index.js
+++ recursos/react/dia_73/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./App.css";
+import "./AppDarkMode.css";
 
 ReactDOM.render(
   <React.StrictMode>
