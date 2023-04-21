df6f0d1a6f1dea4a1333d01e6058203c807efde1
EvananSemprun <evanansemprun@gmail.com>
2023-04-20 21:47:59 -0400

    dia_74-75

--- recursos/react/dia_74-75/src/components/Header.js
+++ recursos/react/dia_74-75/src/components/Header.js
@@ -1,15 +1,52 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
+  const [navActive, setNavActive] = useState(false);
+
+  const toggleNavActive = () => {
+    setNavActive(!navActive);
+  };
+
+  const closeNav = () => {
+    setNavActive(false);
+  };
+
   return (
     <header>
       <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/services">Services</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+        <div className="logo">
+          <Link to="/" onClick={closeNav}>
+            My Logo
+          </Link>
+        </div>
+        <div className="hamburger-menu" onClick={toggleNavActive}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <ul className={navActive ? 'nav-links nav-active' : 'nav-links'}>
+          <li>
+            <Link to="/" onClick={closeNav}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" onClick={closeNav}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/services" onClick={closeNav}>
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" onClick={closeNav}>
+              Contact
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
--- recursos/react/dia_74-75/src/pages/HomePage.js
+++ recursos/react/dia_74-75/src/pages/HomePage.js
@@ -1,11 +1,99 @@
 import React from 'react';
 import './HomePage.css';
+import Slider from 'react-slick';
 
 function HomePage() {
+  const images = [
+    'https://example.com/slide1.jpg',
+    'https://example.com/slide2.jpg',
+    'https://example.com/slide3.jpg'
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000
+  };
+
+  const products = [
+    {
+      id: 1,
+      name: 'Pizza',
+      image: 'https://example.com/pizza.jpg',
+      price: '$9.99'
+    },
+    {
+      id: 2,
+      name: 'Burger',
+      image: 'https://example.com/burger.jpg',
+      price: '$6.99'
+    },
+    {
+      id: 3,
+      name: 'Taco',
+      image: 'https://example.com/taco.jpg',
+      price: '$3.99'
+    }
+  ];
+
+  const testimonials = [
+    {
+      id: 1,
+      name: 'John Smith',
+      image: 'https://example.com/john.jpg',
+      comment: 'I love the food at this restaurant!'
+    },
+    {
+      id: 2,
+      name: 'Sarah Johnson',
+      image: 'https://example.com/sarah.jpg',
+      comment: 'The service here is amazing!'
+    },
+    {
+      id: 3,
+      name: 'Mike Lee',
+      image: 'https://example.com/mike.jpg',
+      comment: 'The prices are so affordable!'
+    }
+  ];
+
   return (
     <div className="home">
-      <h1>Welcome to our website!</h1>
-      <p>We are a company that provides excellent services.</p>
+      <Slider {...settings}>
+  {images.map(image => (
+    <div key={image}>
+      <img src={image} alt="Slider" />
+    </div>
+  ))}
+</Slider>
+      <div className="content">
+        <h2>Featured Products</h2>
+        <div className="products">
+          {products.map(product => (
+            <div key={product.id} className="product">
+              <img src={product.image} alt={product.name} />
+              <div className="product-info">
+                <h3>{product.name}</h3>
+                <p>{product.price}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <h2>Testimonials</h2>
+        <div className="testimonials">
+          {testimonials.map(testimonial => (
+            <div key={testimonial.id} className="testimonial">
+              <img src={testimonial.image} alt={testimonial.name} />
+              <p>{testimonial.comment}</p>
+              <cite>{testimonial.name}</cite>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
